814f9bdd407243ae1ec8f1afbbf7cfece0da8d1c
Lukasz <skotniczny@outlook.com>
2021-06-06 01:58:03 +0200

    Aktualizacja tytułu strony

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/stegano-vue/'
-    : '/'
+    : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Stegano Vue'
+    }
+  }
 }
